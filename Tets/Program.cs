eb1b3fca11069c9e67396c328819fb2e6cd6b87e
Gerson Saymon <gersonsaymon@gmail.com>
2021-04-22 19:58:38 -0300

    programa no inicio, só mostra ola mundo na tela

---
+++ Tets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 10;
+            //int y = 19;
+
+            //Console.WriteLine(testes.Teste(x, y));
+            //testes.Triple(ref x); // operação ref n pode ser feita dentro do writeline.
+            //Console.WriteLine(x);
+
+
+
+            //------------Listas---------
+
+            List<string> list = new List<string>();
+
+            List<string> list2 = new List<string> { "maria", "alex" };
+
+            list.Add("maria");
+            list.Add("Alex");
+            list.Add("bob");
+            list.Add("Anna");
+
+            list.Insert(2, "marco");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("list count: " + list.Count);
+
+
+        }
+    }
+}
---
+++ Tets/testes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tets
+{
+    class testes
+    {
+        public int Soma;
+
+        public static int Teste(int x, int y)
+        {
+             int sum = x + y;
+            return sum;
+        }
+
+        public static void Triple(ref int x)
+        {
+            x *= 3; 
+        }
+
+        public int Somatoria(int soma)
+        {
+            Soma += soma;
+            return Soma;
+        }
+    }
+}
